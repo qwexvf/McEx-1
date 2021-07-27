f8f92e161e717a0d4750dfb02df08df26eb7c789
Marcelo <qw3xvf@gmail.com>
2021-07-27 21:18:22 +0900

    update deps

--- mix.exs
+++ mix.exs
@@ -3,24 +3,24 @@ defmodule McEx.Mixfile do
 
   def project do
     [app: :mc_ex,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: ["lib", "plugins"],
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     #compilers: [:rustler] ++ Mix.compilers,
-     deps: deps,
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
-     rustler_crates: []]
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      elixirc_paths: ["lib", "plugins"],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      #compilers: [:rustler] ++ Mix.compilers,
+      deps: deps,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      rustler_crates: []]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :cutkey, :gproc],
-     mod: {McEx, []}]
+    [applications: [:logger, :gproc],
+      mod: {McEx, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -33,17 +33,18 @@ defmodule McEx.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:poison, "~> 2.1.0"},
-     {:cutkey, github: "imtal/cutkey"},
-     {:gproc, github: "uwiger/gproc"}, #"~> 0.5.0"},
-     {:uuid, "~> 1.1"},
-     {:mc_chunk, github: "McEx/McChunk"},
-     {:mc_protocol, github: "McEx/McProtocol"},
-     {:rustler, "~> 0.0.7"},
-     # Dev
-     {:credo, "~> 0.3", only: [:dev, :test]},
-     {:excoveralls, "~> 0.5", only: :test},
-     {:dialyxir, "~> 0.3", only: [:dev]},
-     {:inch_ex, ">= 0.0.0", only: :docs}]
+    [
+      {:poison, "~> 2.1.0"},
+      {:gproc, github: "uwiger/gproc"}, #"~> 0.5.0"},
+      {:uuid, "~> 1.1"},
+      {:mc_chunk, github: "McEx/McChunk"},
+      {:mc_protocol, github: "McEx/McProtocol"},
+      {:rustler, "~> 0.21.0"},
+      # Dev
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]}
+    ]
   end
 end
